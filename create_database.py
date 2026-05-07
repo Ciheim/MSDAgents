b2ea07c6ff9255a59865f2de015f4c92da050269
mlee03 <Mikyung.Lee@lscamd50-d.gfdl.noaa.gov>
2026-05-07 17:04:25 -0400

    update

--- create_database.py
+++ create_database.py
@@ -1,39 +1,107 @@
 import importlib
-import importlib.util
 import inspect
-import click
+import re
+from types import SimpleNamespace
+
+import click        
 
 class ModuleDocument():
 
     def __init__(self, modulename: str, packagename: str = "fre"):
-
+        """
+        Class to convert docstrings in a module to a report format
+        Example use case:
+        self.packagename = "fre"
+        self.modulename = "fre.make.fremake"
+        self.documentation.overview = "This module contains functions to run the fremake script."
+        self.documentation.functions = {
+            "run_fremake_script": {
+            "overview": "This function runs the fremake script."
+            "params": [
+                "src_dir is the absolute directory path to git clone the source code. src_dir is of type str."
+            ],
+            "raises": [
+                "ValueError is raised when platform does not exist in platforms.yaml."
+            ],
+            "notes": "Note: This function is a wrapper around the fremake script."
+            }
+        }
+        self.documentation.overview = "This module contains functions to run the fremake script."
+        self.documentation.functions = {
+            "run_fremake_script": "This function runs the fremake script.
+             The function has the following parameters: src_dir is the absolute directory path to git clone the source code. 
+             src_dir is of type str. 
+             The function can raise the following exceptions: ValueError is raised when platform does not exist in platforms.yaml. 
+             Note: This function is a wrapper around the fremake script."
+        }
+        self.metadata = {
+            "run_fremake_script": {
+                "name": "run_fremake_script",
+                "module": "fre.make.fremake",
+                "package": "fre"
+            }
+        }
+        """
         self.packagename = packagename
         self.modulename = modulename
-        self.documents = {"functions": {}, "overview": None}
+        self.metadata = {} 
+        self.overview = None
+        self.doc_dict = {} 
+        self.documentation = SimpleNamespace(
+            overview = None,
+            functions = None
+        )
 
         #sys.path.insert(0, str(Path(modulename).parent))
         self.mod = importlib.import_module(self.modulename)
 
-    def docstrings_to_sentences(self):
+    def docstrings_to_report(self):
         """
-        Parses the module file and saves docstrings into self.documents
+        Parses the module file and saves docstrings into this object.
         """
 
-        self.documents["overview"] = self._check(inspect.getdoc(self.mod))
+        self.documentation.overview = self._check(inspect.getdoc(self.mod))
+        self.documentation.functions = {}
 
         for name, func in inspect.getmembers(self.mod, inspect.isfunction):
             if inspect.getmodule(func) is not self.mod:
                 continue
-
+                
             docstring = inspect.getdoc(func)
             docstring_dict = self.split_docstring(docstring)        
 
-            self.documents["functions"][name] = {
+            doc_dict= {
                 "overview": self._clean(docstring_dict["overview"]),
                 "params": self.parse_params(docstring_dict["params"]),
                 "raises": self.parse_raises(docstring_dict["raises"]),
                 "notes": self.parse_notes(docstring_dict["notes"])
             }
+            self.doc_dict[name] = doc_dict
+            self.documentation.functions[name] = self.convert_to_reports(doc_dict)
+            self.metadata[name] = {
+                "name": name,
+                "module": self.modulename,
+                "package": self.packagename
+            }
+
+    def convert_to_reports(self, doc_dict):
+        """
+        Converts the sentences in self.functions into a report format.
+        """
+        
+        report = doc_dict["overview"]
+        if doc_dict["params"]:
+            report += " The function has the following parameters: "
+            for param in doc_dict["params"]:
+                report += param
+        if doc_dict["raises"]:
+            report += " The function can raise the following exceptions: "
+            for raise_ in doc_dict["raises"]:
+                report += raise_
+        if doc_dict["notes"]:
+            report += " Note: " + doc_dict["notes"]
+            
+        return report
 
     def split_docstring(self, docstring_in):
         
@@ -75,11 +143,12 @@ class ModuleDocument():
         for param_and_type_string in params_docstring.split(":param"):
             paramstuff, typestring = param_and_type_string.split(":type", 1)
             paramname, paramstring = paramstuff.split(":", 1)
+            type_ = typestring.split(":", 1)[1]
             
             paramname = self._check(paramname)
             params.append(
-                f"{paramname} {self._clean(paramstring)}. "
-                f"{paramname} is of type {self._clean(typestring)}."
+                f"{paramname} {self._clean(paramstring)}.  "
+                f"{paramname} is of type {self._clean(type_)}.  "
             )
 
         return params
@@ -126,25 +195,22 @@ class ModuleDocument():
         """
         for escape_char in ("\n", "\t", "\r", "\b", "\f", "\v", "\0"):
             string_in = string_in.replace(escape_char, "")
+        string_in = re.sub(r' {3,}', '  ', string_in)
         return string_in.strip()
 
     def __repr__(self):
         functions = []
-        for name, content in self.documents["functions"].items():
-            params_str = "\n      ".join(content['params']) if content['params'] else ""
-            raises_str = "\n      ".join(content['raises']) if content['raises'] else ""
-            
-            func_str = f"  {name}:\n"
-            func_str += f"    Overview: {content['overview']}\n"
-            func_str += f"    Params:\n      {params_str}\n" if params_str else "    Params:\n"
-            func_str += f"    Raises:\n      {raises_str}\n" if raises_str else "    Raises:\n"
-            func_str += f"    Notes: {content['notes']}\n\n"
-            functions.append(func_str)
-        
+        for name, report in self.documentation.functions.items():
+            functions.append(f"* {name}:\n{report}\n")
+
+        functions_text = "\n".join(functions)
+
         return (
-            f"MODULE: {self.modulename}\n"
-            f"Overview:\n{self.documents['overview']}\n"
-            f"Functions:\n{''.join(functions)}"
+            "__________________________________\n"
+            f"MODULE:\n{self.modulename}\n\n"
+            f"Overview:\n{self.documentation.overview}\n\n"
+            f"Functions:\n{functions_text}"
+            "__________________________________"
         )
 
 
@@ -154,7 +220,8 @@ class CommandDocument():
 
         self.modulename = modulename
         self.groupcommand = groupcommand
-        self.documents = {"commands": {}, "overview": None}
+        self.functions = {}
+        self.overview = None
 
     def get_click_commands(self) -> list[str]:
         """
@@ -162,6 +229,7 @@ class CommandDocument():
         defined in this module.
         """
         mod = importlib.import_module(self.modulename)
+        self.functions = {}
         
         for name in dir(mod):
             obj = getattr(mod, name)
@@ -169,23 +237,36 @@ class CommandDocument():
                 docstring = inspect.getdoc(obj)
                 ctx = click.Context(obj)    
                 help_output = obj.get_help(ctx)
-                self.documents["commands"][obj.name] = {
-                    "overview": self._clean(docstring),
-                    "help": self._clean(help_output)
+                self.functions[obj.name] = {
+                    "overview": self._clean(self._check(docstring)),
+                    "help": self._check(help_output)
                 }
+
+        return list(self.functions.keys())
+    
+    def _check(self, docstring):
+        """
+        Utility method
+        """
+        if docstring is not None:
+            docstring_stripped = docstring.strip()
+            if docstring_stripped:
+                return docstring_stripped
+        return ""
+
     
     def _clean(self, string_in):
         """
         Utility method
         """
-
         for escape_char in ("\n", "\t", "\r", "\b", "\f", "\v", "\0"):
             string_in = string_in.replace(escape_char, "")
+        string_in = re.sub(r' {3,}', '  ', string_in)
         return string_in.strip()
     
     def __repr__(self):
         commands_str = ""
-        for name, content in self.documents["commands"].items():
+        for name, content in self.functions.items():
             commands_str += f"  {name}:\n"
             commands_str += f"    Overview: {content['overview']}\n"
             commands_str += f"    Help: {content['help']}\n\n"
---
+++ use-create-database.py
@@ -0,0 +1,102 @@
+from create_database import ModuleDocument, CommandDocument
+#import ChromaDB
+
+makemodules = [
+  "fre.make.create_checkout_script",
+  "fre.make.create_compile_script",
+  "fre.make.create_docker_script",
+  "fre.make.create_makefile_script",
+  "fre.make.make_helpers",
+  "fre.make.run_fremake_script"
+]
+yamlmodules = [
+    "fre.yamltools.abstract_classes",
+    "fre.yamltools.combine_yamls_script",
+    "fre.yamltools.constructors",
+    "fre.yamltools.helpers",
+
+]
+appmodules = [
+    "fre.app.generate_time_averages.cdoTimeAverager",
+    "fre.app.generate_time_averages.combine",
+    "fre.app.generate_time_averages.frenctoolsTimeAverager",
+    "fre.app.generate_time_averages.frepytoolsTimeAverager",
+    "fre.app.generate_time_averages.generate_time_averages",
+    "fre.app.generate_time_averages.timeAverager",
+    "fre.app.generate_time_averages.wrapper",
+    "fre.app.mask_atmos_plevel.mask_atmos_plevel",
+    "fre.app.regrid_xy.regrid_xy",
+    "fre.app.remap_pp_components.remap_pp_components",
+]
+listmodules = [
+    "fre.list_.list_experiments_script",
+    "fre.list_.list_platforms_script",
+    "fre.list_.list_pp_components_script",    
+]
+ppmodules = [
+    "fre.pp.checkout_script",
+    "fre.pp.configure_script_yaml",
+    "fre.pp.histval_script",
+    "fre.pp.install_script",
+    "fre.pp.nccheck_script",
+    "fre.pp.ppval_script",
+    "fre.pp.rename_split_script",
+    "fre.pp.run_script",
+    "fre.pp.split_netcdf_script",
+    "fre.pp.status_script",
+    "fre.pp.trigger_script",
+    "fre.pp.validate_script",
+    "fre.pp.wrapper_script"
+]
+runmodules = [
+    "fre.run.frerunexample"
+]
+
+makecommands = [{"modulename": "fre.make.fremake", "groupcommand": "fre make"}]
+yamlcommands = [{"modulename": "fre.yamltools.freyamltools", "groupcommand": "fre yamltools"}]
+appcommands = [{"modulename": "fre.app.freapp", "groupcommand": "fre app"}]
+catalogcommands = [{"modulename": "fre.catalog.frecatalog", "groupcommand": "fre catalog"}]
+listcommands = [{"modulename": "fre.list_.frelist", "groupcommand": "fre list"}]
+ppcommands = [{"modulename": "fre.pp.frepp", "groupcommand": "fre pp"}]
+runcommands = [{"modulename": "fre.run.frerun", "groupcommand": "fre run"}]
+
+#schemas
+#mkmf
+
+allmodules = [
+    makemodules,
+    yamlmodules,
+    appmodules,
+    listmodules,
+    ppmodules,
+    runmodules
+]
+allcommands =[
+    makecommands,
+    yamlcommands,
+    appcommands,
+    catalogcommands,
+    listcommands,
+    ppcommands,
+    runcommands
+]
+
+documents = {}
+for module_group in allmodules:
+    for amodule in module_group:
+        try:
+            document = ModuleDocument(amodule)
+            document.docstrings_to_report()
+            documents[amodule] = document
+        except Exception as exc:
+            print(f"Skipping module {amodule}: {exc}")
+
+clickdocuments = {}
+for command_group in allcommands:
+    for command in command_group:
+        try:
+            document = CommandDocument(**command)
+            document.get_click_commands()
+            clickdocuments[command["modulename"]] = document
+        except Exception as exc:
+            print(f"Skipping command group {command['modulename']}: {exc}")
